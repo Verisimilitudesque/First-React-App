65363b03ada484b0eb443635d949c7cf534f090e
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-14 08:39:37 -0600

    App code and file clean up, component testing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client"
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Accordion from 'react-bootstrap/Accordion';
+
+const root = createRoot(document.getElementById("root"))
+const reactElement = <h1>Hello from JSX! Poop</h1>
+
+console.log(reactElement)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    reactElement
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Basic React Component
+function MainContent() {
+  return (
+    
+    <h1>React is great!</h1>
+  );
+}
+// Export default MainContent;
+export default MainContent;
+
+const mainContent = createRoot(document.getElementById("accordion"))
+//Rendering the MainContent component
+mainContent.render (
+    <div>
+        <MainContent />
+    </div>
+)
