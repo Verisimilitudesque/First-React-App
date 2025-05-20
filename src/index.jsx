fe8d5b13c4f98301d15d93902d3278fc945acb89
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-20 09:50:41 -0600

    Rearranged app files, tested component with html, h1 header tags and img tags

---
+++ src/index.jsx
@@ -0,0 +1,58 @@
+// import { createRoot } from "react-dom/client"
+import './index.css';
+
+// const root = createRoot(document.getElementById("root"))
+// const reactElement = <h1>Hello from JSX!</h1>
+
+// root.render(
+//     reactElement
+// )
+
+//Basic React Component
+// function MainContent() {
+//   return (
+    
+//     <h1>React is great!</h1>
+//   );
+// }
+// // Export default MainContent;
+// export default MainContent;
+
+// const mainContent = createRoot(document.getElementById("accordion"))
+// //Rendering the MainContent component
+// mainContent.render (
+//     <div>
+//         <MainContent />
+//     </div>
+// )
+
+
+// This is imperative coding
+// Creates new element
+//const h1 = document.createElement("h1")
+// Appends text to the element
+//h1.textContent = "This is imperative coding"
+// Adds css class to the element
+//h1.className = "header"
+// Renders the element to the DOM
+//document.getElementById("root").appendChild(h1)
+
+
+// Add image and html in React 
+
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <main>
+        <img src="../public/logo192.png" width="40px" alt="react logo" />
+        <h1>Fun facts about React!</h1>
+        <ul>
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
